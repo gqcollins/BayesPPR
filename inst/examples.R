e7602a0ce5f14b873a6e84248c94fa0a4946807b
gqcollins <gqcollins@gmail.com>
2022-05-31 16:47:08 -0400

    Fix example

--- inst/examples.R
+++ inst/examples.R
@@ -1,7 +1,7 @@
 \dontrun{
-  ####################################################################################################
+  ################################
   ### univariate example
-  ####################################################################################################
+  ################################
   ## simulate data (Friedman function)
   f <- function(X){
     10*sin(pi*X[,1]*X[,2]) + 20*(X[,3] - .5)^2 + 10*X[,4] + 5*X[,5]
@@ -15,12 +15,12 @@
   fit <- bppr(X, y)
 
   ## prediction
-  X_test <- matrix(runif(npred*p), n, p)
-  preds <- predict(mod, X_test, n_ignore = 9000) # posterior predictive samples
+  X_test <- matrix(runif(n*p), n, p)
+  preds <- predict(fit, X_test, n_ignore = 9000) # posterior predictive samples
   true_f <- f(X_test)
   plot(true_f, colMeans(preds), xlab = 'true values', ylab='posterior predictive means')
   abline(a=0, b=1, col=2)
 }
 
 ## minimal example for CRAN testing
-fit <- bppr(1:2, 1:2, n_draws = 2)
+fit <- bppr(matrix(1:10), 1:10, n_draws = 2)
